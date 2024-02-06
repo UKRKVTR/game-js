5c95bae129a14d1f188fa929d1c9272121fad25c
Danylo <warfais6@gmail.com>
2024-02-06 13:45:47 +0200

    pr 6_2

--- main.js
+++ main.js
@@ -1,71 +1,83 @@
-const $btnKick = document.getElementById('btn-kick')
-const $btnSpecial = document.getElementById('btn-special')
+const $btnKick = document.getElementById("btn-kick");
+const $btnSpecial = document.getElementById("btn-special");
 
 const fighter = {
-  character: {
-    name: 'Pikachu',
-    defaultHP: 100,
-    damageHP: 100,
-    elHP: document.getElementById('health-character'),
-    elProgressbar: document.getElementById('progressbar-character')
-  },
-  enemy: {
-    name: 'Charmander',
-    defaultHP: 100,
-    damageHP: 100,
-    elHP: document.getElementById('health-enemy'),
-    elProgressbar: document.getElementById('progressbar-enemy')
-  }
-}
+    character: {
+        name: 'Pikachu',
+        defaultHP: 100,
+        damageHP: 100,
+        elHP: document.getElementById("health-character"),
+        elProgressbar: document.getElementById("progressbar-character"),
+        renderHP: function () {
+            this.renderHPLife();
+            this.renderProgressbarHP();
+        },
+        renderHPLife: function () {
+            this.elHP.innerText = this.damageHP + '/' + this.defaultHP;
+        },
+        renderProgressbarHP: function () {
+            this.elProgressbar.style.width = this.damageHP + '%';
+        },
+        changeHP: function (count) {
+            if (this.damageHP < count) {
+                this.damageHP = 0;
+                alert("Бедный " + this.name + " проиграл бой!");
+                $btnKick.disabled = true;
+                $btnSpecial.disabled = true;
+            } else {
+                this.damageHP -= count;
+            }
+            this.renderHP();
+        },
+    },
+    enemy: {
+        name: 'Charmander',
+        defaultHP: 100,
+        damageHP: 100,
+        elHP: document.getElementById("health-enemy"),
+        elProgressbar: document.getElementById("progressbar-enemy"),
+        renderHP: function () {
+            this.renderHPLife();
+            this.renderProgressbarHP();
+        },
+        renderHPLife: function () {
+            this.elHP.innerText = this.damageHP + '/' + this.defaultHP;
+        },
+        renderProgressbarHP: function () {
+            this.elProgressbar.style.width = this.damageHP + '%';
+        },
+        changeHP: function (count) {
+            if (this.damageHP < count) {
+                this.damageHP = 0;
+                alert("Бедный " + this.name + " проиграл бой!");
+                $btnKick.disabled = true;
+                $btnSpecial.disabled = true;
+            } else {
+                this.damageHP -= count;
+            }
+            this.renderHP();
+        },
+    },
+};
 
 $btnKick.addEventListener('click', function () {
-  console.log('Kick')
-  battleRound(20)
-})
+    console.log('Kick');
+    fighter.character.changeHP(20);
+});
 
 $btnSpecial.addEventListener('click', function () {
-  console.log('Special Attack')
-  battleRound(30)
-})
+    console.log('Special Attack');
+    fighter.character.changeHP(30);
+});
 
-function init () {
-  console.log('Start game!')
-  renderHP(fighter.character)
-  renderHP(fighter.enemy)
+function init() {
+    console.log('Start game!');
+    fighter.character.renderHP();
+    fighter.enemy.renderHP();
 }
 
-function renderHP (person) {
-  renderHPLife(person)
-  renderProgressbarHP(person)
+function random(num) {
+    return Math.ceil(Math.random() * num);
 }
 
-function renderHPLife (person) {
-  person.elHP.innerText = person.damageHP + '/' + person.defaultHP
-}
-
-function renderProgressbarHP (person) {
-  person.elProgressbar.style.width = person.damageHP + '%'
-}
-
-function battleRound (damage) {
-  changeHP(damage, fighter.character)
-  changeHP(random(20), fighter.enemy)
-}
-
-function changeHP (count, person) {
-  if (person.damageHP < count) {
-    person.damageHP = 0
-    alert('Бедный ' + person.name + ' проиграл бой!')
-    $btnKick.disabled = true
-    $btnSpecial.disabled = true
-  } else {
-    person.damageHP -= count
-  }
-  renderHP(person)
-}
-
-function random (num) {
-  return Math.ceil(Math.random() * num)
-}
-
-init()
+init();

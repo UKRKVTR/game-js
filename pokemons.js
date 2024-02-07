f6b25881a2e5068cf291da0d2aaa636dd8421402
Danylo <warfais6@gmail.com>
2024-02-07 19:51:11 +0200

    pr 6_6

---
+++ pokemons.js
@@ -0,0 +1,194 @@
+export const pokemons = [
+  {
+    name: 'Pikachu',
+    type: 'electric',
+    hp: 274,
+    img: './assets/pick.png',
+    attacks: [
+      {
+        name: 'thunder jolt',
+        maxDamage: 40,
+        minDamage: 20,
+        maxCount: 100
+      },
+      {
+        name: 'electro ball',
+        maxDamage: 60,
+        minDamage: 45,
+        maxCount: 6
+      },
+      {
+        name: 'volt tackle',
+        maxDamage: 75,
+        minDamage: 60,
+        maxCount: 4
+      },
+      {
+        name: 'thunder crack',
+        maxDamage: 160,
+        minDamage: 130,
+        maxCount: 2
+      }
+    ]
+  },
+  {
+    img: './assets/2pick.png',
+    name: 'Charmander',
+    type: 'fire',
+    hp: 282,
+    attacks: [
+      {
+        name: 'ember',
+        maxDamage: 40,
+        minDamage: 20,
+        maxCount: 100
+      },
+      {
+        name: 'flamethrower',
+        maxDamage: 60,
+        minDamage: 45,
+        maxCount: 6
+      },
+      {
+        name: 'burning tail',
+        maxDamage: 75,
+        minDamage: 60,
+        maxCount: 4
+      },
+      {
+        name: 'fire spin',
+        maxDamage: 130,
+        minDamage: 110,
+        maxCount: 2
+      }
+    ]
+  },
+  {
+    img: '/assets/pngegg.png',
+    name: 'Bulbasaur',
+    type: 'fire',
+    hp: 294,
+    attacks: [
+      {
+        name: 'tackle',
+        maxDamage: 40,
+        minDamage: 20,
+        maxCount: 100
+      },
+      {
+        name: 'vine whip',
+        maxDamage: 60,
+        minDamage: 45,
+        maxCount: 6
+      },
+      {
+        name: 'razor leaf',
+        maxDamage: 75,
+        minDamage: 60,
+        maxCount: 4
+      },
+      {
+        name: 'solar beam',
+        maxDamage: 130,
+        minDamage: 110,
+        maxCount: 2
+      }
+    ]
+  },
+  {
+    img: '/assets/Squirtle.png',
+    name: 'Squirtle',
+    type: 'water',
+    hp: 292,
+    attacks: [
+      {
+        name: 'bubble',
+        maxDamage: 40,
+        minDamage: 20,
+        maxCount: 100
+      },
+      {
+        name: 'water gun',
+        maxDamage: 60,
+        minDamage: 45,
+        maxCount: 6
+      },
+      {
+        name: 'shell attack',
+        maxDamage: 75,
+        minDamage: 60,
+        maxCount: 4
+      },
+      {
+        name: 'hydro pump',
+        maxDamage: 130,
+        minDamage: 110,
+        maxCount: 2
+      }
+    ]
+  },
+  {
+    img: '/assets/Leafeon.png',
+    name: 'Leafeon',
+    type: 'Earth',
+    hp: 284,
+    attacks: [
+      {
+        name: 'Earth Slash',
+        maxDamage: 40,
+        minDamage: 20,
+        maxCount: 100
+      },
+      {
+        name: 'Wind',
+        maxDamage: 60,
+        minDamage: 45,
+        maxCount: 6
+      },
+      {
+        name: 'Heat Wave',
+        maxDamage: 75,
+        minDamage: 60,
+        maxCount: 4
+      },
+      {
+        name: 'Attack',
+        maxDamage: 130,
+        minDamage: 110,
+        maxCount: 2
+      }
+    ]
+  },
+  {
+    img: '/assets/Haunter .png',
+    name: 'Haunter',
+    type: 'Gas',
+    hp: 500,
+    attacks: [
+      {
+        name: 'Poison',
+        maxDamage: 40,
+        minDamage: 20,
+        maxCount: 100
+      },
+      {
+        name: 'Poison Power',
+        maxDamage: 60,
+        minDamage: 45,
+        maxCount: 6
+      },
+      {
+        name: 'Aura Sphere',
+        maxDamage: 75,
+        minDamage: 60,
+        maxCount: 4
+      },
+      {
+        name: 'Psychic',
+        maxDamage: 130,
+        minDamage: 110,
+        maxCount: 2
+      }
+    ]
+  }
+]
